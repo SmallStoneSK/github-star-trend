06238bcce32049a41fde3941a3e300242dce3ed8
SmallStoneSK <575913857@qq.com>
2019-06-15 20:48:05 +0800

    [modify] modify npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: path.join(__dirname, './src/injected.js'),
   output: {
     path: path.join(__dirname, './dist'),
